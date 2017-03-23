a5dd47f3a136d0c854678d899bc7368a37b194cd
quenmasc <quent-200193@hotmail.fr>
2017-03-23 15:59:54 -0400

    add range in PlotGMM function argument

--- Gaussian_Mixture_Model/PlotGMM/PlotGMM.m
+++ Gaussian_Mixture_Model/PlotGMM/PlotGMM.m
@@ -1,10 +1,13 @@
-function PlotGMM(model)
+function PlotGMM(model,Range)
     if (size(model.mu,1)>2)
         disp('Error of dimension');
         return;
+    elseif (length(Range)<2)
+        disp('Error ! Range need to have 2 values');
+        return;
     else
         obj = gmdistribution((model.mu).',model.sigma,model.weight);
-        ezsurf(@(x,y)pdf(obj,[x y]),[-5 5],[-5 5]);
+        ezsurf(@(x,y)pdf(obj,[x y]),Range,Range);
     end
         
 end
