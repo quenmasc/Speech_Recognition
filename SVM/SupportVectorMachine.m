ffda81205ddd2d9d162b760580f7826741fcaa6d
quenmasc <quent-200193@hotmail.fr>
2017-03-28 13:32:36 -0400

    feature permuted

--- SVM/SupportVectorMachine.m
+++ SVM/SupportVectorMachine.m
@@ -5,16 +5,17 @@ function espace=SupportVectorMachine()
     clc ;
     
     %% import data
-    imageDir = fullfile('../Data/features/features_saved/ALL/');
+    imageDir = fullfile('../Data/features/features_filtered/ALL/');
     filenames = dir(fullfile(imageDir, '*.mat'));
     features=zeros(numel(filenames),5850);
     Output=zeros(numel(filenames),1);
         for i=1:numel(filenames)
             path=strcat(filenames(i).folder,'/',filenames(i).name);
             im=load(path); 
-            features(i,:)=im.local_feature;
+            features(i,:)=im.local_feature_w;
             Output(i)=im.local_classe;
         end
+        keyboard;
         t = templateSVM('Standardize',1,'KernelFunction','rbf','KernelScale','auto');
 % t = templateSVM('Standardize',1,'KernelFunction','rbf');
 
