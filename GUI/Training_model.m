18407c9af95648130dec545aa4c5616928d31a3e
quenmasc <quent-200193@hotmail.fr>
2017-03-28 10:13:08 -0400

    add button leave and complete function too

--- GUI/Training_model.m
+++ GUI/Training_model.m
@@ -22,7 +22,7 @@ function varargout = Training_model(varargin)
 
 % Edit the above text to modify the response to help Training_model
 
-% Last Modified by GUIDE v2.5 13-Mar-2017 12:32:50
+% Last Modified by GUIDE v2.5 28-Mar-2017 10:02:41
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -1059,3 +1059,11 @@ function textbox_name_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+% --- Executes on button press in button_leave.
+function button_leave_Callback(hObject, eventdata, handles)
+% hObject    handle to button_leave (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+    close all;
