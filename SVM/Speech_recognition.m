5dd4cf7e39cf519aa2825ec46bfd708f27a79878
quenmasc <quent-200193@hotmail.fr>
2017-04-03 11:45:52 -0400

    try to make three SVM, one to determine short or long word, the two other which classify word in context

--- SVM/Speech_recognition.m
+++ SVM/Speech_recognition.m
@@ -1,4 +1,4 @@
-function r=Speech_recognition(Mdl)
+function r=Speech_recognition(Mdl_classe,Mdl_short,Mdl_long)
     %% addpath
     addpath('../Ressources');
     addpath('../Vocal_Activity_Detection_Algorithm');
@@ -8,7 +8,13 @@ function r=Speech_recognition(Mdl)
     labelsave=[];
     for i=1:length(segments)
     [features,~]=SetFeactureExtraction(cell2mat(segments(i)),fs,15,5);
-    [label,score] = predict(Mdl,features);
+    [label,score] = predict(Mdl_classe,features);
+    if (label==1)
+        new_feature=features(1,1:3000);
+        [label,score] = predict(Mdl_short,new_feature);
+    else
+        [label,score] = predict(Mdl_long,features);
+    end
     labelsave=[labelsave,label];
     end
 
