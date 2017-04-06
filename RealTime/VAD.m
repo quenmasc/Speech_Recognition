137ab035d404b33bc4a68fb331a7c884e9c0733b
quenmasc <quent-200193@hotmail.fr>
2017-04-06 17:09:16 -0400

    VAD realtime optimization

---
+++ RealTime/VAD.m
@@ -0,0 +1,70 @@
+%% MASCRET Quentin - Copyright 2017 
+% This function allow to extract speech in a recording. 
+%% DATE 02-24-17
+%% Maj 1.2
+%
+%% INPUT 
+% Signal = vector of 1 x n dimension -> mono
+% window_ms= length of frame in ms
+% step_ms = step between two windows in ms
+% fs = sample rate 
+%
+%% OUTPUT 
+% Segments : segments of speech extraction
+%
+function [segments,Signal,Limits,fs]=Vocal_algorithm_dectection(Init,time_record,fs)
+
+    %% add path
+    addpath('source/');
+    % variables
+    weight=2;
+    step_ms= 5;% fixed by default 
+    window_ms = 15; % fixed by default
+    
+
+    % record
+    label2=strcat('You can speak for ~',num2str(time_record),' s.');
+    [Signal,~]=recorder(label2,time_record);
+    
+    % conversion ms to samples
+    step_sample =floor(step_ms*10^-3*fs);
+    window_sample=floor(window_ms*10^-3*fs);
+    nbFrame=floor((numel(Signal)-window_sample)/step_sample);
+    L=nwindow_sample;
+    N=2^(nextpow2(window_sample));
+   % Signal=Signal-mean(Signal);
+   % Signal=Signal/max(abs(Signal));
+    %% tool  %%
+    %% Pre-traitment in order to suppress percussive noise
+    for w=1:nbFrame
+        localFrame =Signal(1+(w-1)*step_sample):((w-1)*step_sample+window_sample);
+        
+        %% Discrete Fourier Transform
+        stft=abs(fft(localFrame.* hamming(L),N));
+        
+        %% MFCCs
+        coeff=mfcc(stft,fs);
+        
+        %% Entropy
+        entropy=spectral_entropy(localFrame,N);
+        
+        %% background noise vector
+        
+        
+    end
+    
+    th=sigmoide_function(10,endpoint);
+    th_e_noise=mean(entropy(1:20))+3*std(entropy(1:20));
+    th_e=variable_threshold(entropy,th_e_noise,0.96);
+ 
+    flags=(endpoint>th)&(entropy>th_e);
+
+    
+
+    Flags_k=flags;
+    %% Segments
+    [segments, Limits, ~, ]=Segmentation_of_voiced_on_input_signal(Flags_k,Signal,weight,window_sample,step_sample,fs);
+
+
+end
+
