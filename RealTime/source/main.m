6e089de8dac4e88e156c72e6eeb80b1e122f662e
quenmasc <quent-200193@hotmail.fr>
2017-04-06 16:58:41 -0400

    try a realtime recording

--- RealTime/source/main.m
+++ RealTime/source/main.m
@@ -1,12 +1,13 @@
 
-global buffer;
+global buffer 
+
 winsize=0.015;
 winshift=0.005;
 T = 0.5; % in seconds
 samples =cell(100,1);
 r = audiorecorder(16000,16,1);
 k=1;
-r.TimerPeriod = 0.2;
+r.TimerPeriod = 0.5;
 r.StopFcn = 'disp(''Completed sample '', k)';
 r.TimerFcn = {@get_pitch,samples{k},winsize,winshift};
 sl=[];
@@ -16,18 +17,9 @@ while 1
      recordblocking(r,T);
      samples{k} =  getaudiodata(r);
      buffer=cell2mat(samples(k));
-     sl=[sl; buffer];
-     figure(1);
-     plot(sl);
-     if (mean(buffer)<=0.01)
-         count=count+1;
-         if (count>=10)
-             break;
-         end
-     else 
-         count=0;
-     end
 end  
+
+
 
 
 
