063cf801829f777886847b5a2af6abfdb2f260db
quenmasc <quent-200193@hotmail.fr>
2017-04-17 18:00:06 -0400

    _

--- SVM/Speech_recognition.m
+++ SVM/Speech_recognition.m
@@ -4,10 +4,10 @@ function [r]=Speech_recognition(Mdl_classe,Mdl_short,Mdl_long)
     addpath('../Vocal_Activity_Detection_Algorithm');
     addpath('../Feature_Extraction/');
     Init=environment_analysis();
-    [segments,~,~,fs]=Vocal_algorithm_dectection(Init,2,16000);
+    [segments,~,~,fs]=Vocal_algorithm_dectection(Init,2,8000);
     labelsave=[];
     for i=1:length(segments)
-    [~,features]=SetFeactureExtraction(cell2mat(segments(i)),fs,15,5);
+    [~,features]=SetFeactureExtraction(cell2mat(segments(i)),fs,25,10);
     [label,score] = predict(Mdl_classe,features);
     if (label==1)
         new_feature=features(1,1:3000);
